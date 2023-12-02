6d0434603f0bedc2e2bb74ed2f894e35db294430
Chris Conyers <conyerch@bc.edu>
2023-12-02 18:23:25 -0500

    first commit

---
+++ college-scorecard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  college-scorecard
+//
+//  Created by christopher conyersIV on 12/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
